8d926324300e7a4b309bf784400e588797d1d724
BrandonHoeft <brandonmhoeft@gmail.com>
2019-03-24 17:41:18 -0500

    updated section 6 app. Moved section 3-4 py files into new directories.

--- udemy/complete_python_and_postgres_dev_course/section6_movie_system/app.py
+++ udemy/complete_python_and_postgres_dev_course/section6_movie_system/app.py
@@ -1,11 +1,10 @@
 import sys
 from os.path import expanduser
-from movie import Movie
-from user import User
-
 home = expanduser("~")
 desktop_rel_path = '/Desktop/MOOC_work/udemy/complete_python_and_postgres_dev_course/section6_movie_system'
 sys.path.insert(0, home+desktop_rel_path)
+from movie import Movie
+from user import User
 
 user = User('Brandon')
 print(user) # This address is the memory address of where the user obj has been created and stored in memory.
